5678a019a31534f4293f1c694b88adc79261f6d1
davidgcs <dgomezcs94@gmail.com>
2017-05-02 01:21:25 +0200

    Head formateado.

--- application/views/templates/head.php
+++ application/views/templates/head.php
@@ -4,15 +4,19 @@
 	<meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <!--STYLES-->
 	<link href="<?=base_url()?>assets/css/bootstrap.min.css" rel="stylesheet">
 	<link href="<?=base_url()?>assets/css/pizzeria.css" rel="stylesheet">
-	<script src="<?=base_url()?>assets/js/jquery.min.js">
-        </script>
-	<script src="<?=base_url()?>assets/js/bootstrap.min.js"></script>
-    <!--[if lte IE 8]><script src="<?=base_url()?>assets/js/ie/html5shiv.js"></script><![endif]-->
-    <link rel="stylesheet" href="<?=base_url()?>assets/css/main.css" />
-    <!--[if lte IE 8]><link rel="stylesheet" href="<?=base_url()?>assets/css/ie8.css" /><![endif]-->
-    <!--[if lte IE 9]><link rel="stylesheet" href="<?=base_url()?>assets/css/ie9.css" /><![endif]-->
+    <link rel="stylesheet" href="<?=base_url()?>assets/css/main.css">
+
+    <!--SCRIPTS-->
+    <script src="<?=base_url()?>assets/js/jquery.min.js"></script>
+    <script src="<?=base_url()?>assets/js/bootstrap.min.js"></script>
+    <script src="<?=base_url()?>assets/js/jquery.scrollex.min.js"></script>
+    <script src="<?=base_url()?>assets/js/jquery.scrolly.min.js"></script>
+    <script src="<?=base_url()?>assets/js/main.js"></script>
+    <script src="<?=base_url()?>assets/js/skel.min.js"></script>
+    <script src="<?=base_url()?>assets/js/util.js"></script>
 	<title>PizHub</title>
 </head>
 <body onload="<?= (isset($head['onload']) ? $head['onload'] : '') ?>">
