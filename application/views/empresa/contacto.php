6c7e9ff96d5d7da5c6ca0c6b2176ad8a12f88acf
alumno <alumno@TALLER3-8>
2017-05-03 12:20:38 +0200

    # WARNING: head commit changed in the meantime
    maquetar contacto

--- application/views/empresa/contacto.php
+++ application/views/empresa/contacto.php
@@ -1,15 +1,18 @@
 <?php
 
 ?>
-<form action="<?= base_url()?>empresa/enviarMail" method="post">
-    <div>
-        <input type="text" name="nombre" placeholder="Nombre" required>
-    </div>
-    <div>
-        <input type="email" name="email" placeholder="Email" required>
-    </div>
-    <div>
-        <textarea name="mensaje" id="mensaje" cols="30" rows="10" placeholder="Comentarios" required></textarea>
-    </div>
-    <input type="submit" value="Enviar">
-</form>
+<div id="container">
+	<h1>Contacto</h1>
+	<form id="formContacto" action="<?= base_url()?>empresa/enviarMail" method="post">
+	    <div>
+	        <input type="text" name="nombre" placeholder="Nombre" required>
+	    </div>
+	    <div>
+	        <input type="email" name="email" placeholder="Email" required>
+	    </div>
+	    <div>
+	        <textarea name="mensaje" id="mensaje" cols="30" rows="10" placeholder="Comentarios" required></textarea>
+	    </div>
+	    <input id="submit" type="submit" value="Enviar">
+	</form>
+</div>
