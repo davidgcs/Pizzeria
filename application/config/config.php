91433e3bb3c174d84ae52993de4abad2608e694d
Daniel Muñoz <danielmunozmonte@gmail.com>
2017-05-10 01:04:23 +0200

    cambio en el .gitignore

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://davinxy.sytes.net/pizhub/';
+$config['base_url'] = 'http://localhost/pizzeria/';
 
 /*
 |--------------------------------------------------------------------------
