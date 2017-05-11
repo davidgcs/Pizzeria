3182744a8c8c3e2cd9d90d334108fc6432fc0e26
Daniel Muñoz <danielmunozmonte@gmail.com>
2017-05-12 01:08:09 +0200

    fix Usuario_model.php

--- application/models/Usuario_model.php
+++ application/models/Usuario_model.php
@@ -1,84 +1,24 @@
-<<<<<<< HEAD:application/models/Usuario_model.php
 <?php
 class Usuario_model extends CI_Model {
+    public function crearUsuario($nombre,$apellidos,$telefono,$email,$alias,$contraseña) {
+        if(!$this->existeUsuario($alias) && !$this->existeEmail($email)){
 
-	public function crearUsuario($nombre,$apellidos,$telefono,$email,$alias,$contraseña) {
-		if(!$this->existeUsuario($alias) && !$this->existeEmail($email)){
-		
-			$usuario = R::dispense("usuario");
-
-			$usuario["nombre"] = $nombre;
-			$usuario["apellidos"] = $apellidos;
-			$usuario["telefono"] = $telefono;
-			$usuario["email"] = $email;
-			$usuario["alias"] = $alias;
-			$usuario["password"] = md5($contraseña);
-
-			R::store($usuario);
-
-			return true;
-		}
-		else{
-            //Notificar al usuario que el usuario ya existe..
-            return false;
-		}
-	}
-
-	public function login($usuario,$contraseña){
-        if($this->existeUsuario($usuario)){
-            //Se encontró el alias!!
-            return $this->comprobarContraseña("alias",$usuario,$contraseña);
-        }
-        else if($this->existeEmail($usuario)){
-            //Se encontró el email!!
-            return $this->comprobarContraseña("email",$usuario,$contraseña);
+            $usuario = R::dispense("usuario");
+            $usuario["nombre"] = $nombre;
+            $usuario["apellidos"] = $apellidos;
+            $usuario["telefono"] = $telefono;
+            $usuario["email"] = $email;
+            $usuario["alias"] = $alias;
+            $usuario["password"] = md5($contraseña);
+            R::store($usuario);
+            return true;
         }
         else{
-            //no se encuentra el alias ni el correo en la bbdd
+            //Notificar al usuario que el usuario ya existe..
             return false;
         }
     }
-
-    private function existeUsuario($alias) {
-		return R::findOne ( 'usuario', 'alias = ?', [$alias] ) != null ? true : false;
-	}
-
-	private function existeEmail($email){
-        return R::findOne('usuario', 'email = ?', [$email]) != null ? true : false;
-    }
-
-    private function comprobarContraseña($tipoUsuario,$usuario,$contraseña){
-        $pass =  R::findOne('usuario',"$tipoUsuario = '$usuario'")->password;
-        return md5($contraseña) == $pass ? true : false;
-    }
-}
-=======
-<?php
-class Usuario_model extends CI_Model {
-
-	public function crearUsuario($nombre,$apellidos,$telefono,$email,$alias,$contraseña) {
-		if(!$this->existeUsuario($alias) && !$this->existeEmail($email)){
-		
-			$usuario = R::dispense("usuario");
-
-			$usuario["nombre"] = $nombre;
-			$usuario["apellidos"] = $apellidos;
-			$usuario["telefono"] = $telefono;
-			$usuario["email"] = $email;
-			$usuario["alias"] = $alias;
-			$usuario["password"] = md5($contraseña);
-
-			R::store($usuario);
-
-			return true;
-		}
-		else{
-            //Notificar al usuario que el usuario ya existe..
-            return false;
-		}
-	}
-
-	public function login($usuario,$contraseña){
+    public function login($usuario,$contraseña){
         if($this->existeUsuario($usuario)){
             //Se encontró el alias!! devolvemos el alias
             return $this->comprobarContraseña("alias",$usuario,$contraseña);
@@ -92,15 +32,12 @@ class Usuario_model extends CI_Model {
             return false;
         }
     }
-
     private function existeUsuario($alias) {
-		return R::findOne ( 'usuario', 'alias = ?', [$alias] ) != null ? true : false;
-	}
-
-	private function existeEmail($email){
+        return R::findOne ( 'usuario', 'alias = ?', [$alias] ) != null ? true : false;
+    }
+    private function existeEmail($email){
         return R::findOne('usuario', 'email = ?', [$email]) != null ? true : false;
     }
-
     private function comprobarContraseña($tipoUsuario,$usuario,$contraseña){
         $pass =  R::findOne('usuario',"$tipoUsuario = '$usuario'")->password;
         if(md5($contraseña) == $pass){ //contraseña correcta
@@ -115,11 +52,9 @@ class Usuario_model extends CI_Model {
             return null;
         }
     }
-
     private function getUserAlias($email){
         return R::findOne('usuario', 'email = ?', [$email])->alias;
     }
-
     public function getPrimaryKeys(){
         $usuarios = R::findAll("usuario");
         $users = [];
@@ -131,5 +66,4 @@ class Usuario_model extends CI_Model {
         return $users;
     }
 }
->>>>>>> 8414a439ed159a18f6c74713ddfc5ad7f8de12d1:application/models/usuario_model.php
 ?>
