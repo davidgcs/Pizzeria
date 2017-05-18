0bab1911dade1c0cfa68b34205dc4e75d69c3d06
Daniel Domínguez <padawan@doersdf.com>
2017-05-18 17:20:51 +0200

    arreglar ruta css pag actual

--- application/views/templates/head.php
+++ application/views/templates/head.php
@@ -9,7 +9,9 @@
 	<link rel="stylesheet" href="<?=base_url()?>assets/css/pizzeria.css">
     <link rel="stylesheet" href="<?=base_url()?>assets/css/main.css">
     <link rel="stylesheet" href="<?=base_url()?>assets/css/jquery-ui.min.css">
-    <link rel="stylesheet" href="<?=base_url()?>assets/css/<?=uri_string()?>.css"> <!-- CSS de la página actual-->
+    <?php if(file_exists(base_url()."assets/css/".uri_string().".css")):?>
+    <link rel="stylesheet" href="<?=base_url()."assets/css/".uri_string().".css"?>"> <!-- CSS de la página actual-->
+    <?php endif;?>
     <link rel="icon" href="<?=base_url()?>assets/images/LogoCircular.png" />
 
 
