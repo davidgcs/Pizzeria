53ee1fa039419d78fc4df47d0d9ee6abe7c843d7
David Gomez <padawan@doersdf.com>
2017-05-19 11:53:13 +0200

    Añadidas las imagenes de pizzas y la estructura básica de la página

--- application/views/carta/pizzas.php
+++ application/views/carta/pizzas.php
@@ -1,1 +1,46 @@
-<h1>PIZZAS</h1>
+<div class="container">
+    <div class="row">
+        <ul id="pizzaContainer">
+            <li class="pizza" id="p1">
+                <img src="<?= base_url() ?>assets/images/pizza1.png" alt="">
+                <button type="button">Añadir</button>
+            </li>
+            <li class="pizza" id="p2">
+                <img src="<?= base_url() ?>assets/images/pizza2.png" alt="">
+                <button type="button">Añadir</button>
+            </li>
+            <li class="pizza" id="p3">
+                <img src="<?= base_url() ?>assets/images/pizza3.png" alt="">
+                <button type="button">Añadir</button>
+            </li>
+            <li class="pizza" id="p4">
+                <img src="<?= base_url() ?>assets/images/pizza4.png" alt="">
+                <button type="button">Añadir</button>
+            </li>
+            <li class="pizza" id="p5">
+                <img src="<?= base_url() ?>assets/images/pizza5.png" alt="">
+                <button type="button">Añadir</button>
+            </li>
+            <li class="pizza" id="p6">
+                <img src="<?= base_url() ?>assets/images/pizza6.png" alt="">
+                <button type="button">Añadir</button>
+            </li>
+            <li class="pizza" id="p7">
+                <img src="<?= base_url() ?>assets/images/pizza7.png" alt="">
+                <button type="button">Añadir</button>
+            </li>
+            <li class="pizza" id="p8">
+                <img src="<?= base_url() ?>assets/images/pizza8.png" alt="">
+                <button type="button">Añadir</button>
+            </li>
+            <li class="pizza" id="p9">
+                <img src="<?= base_url() ?>assets/images/pizza9.png" alt="">
+                <button type="button">Añadir</button>
+            </li>
+            <li class="pizza" id="p10">
+                <img src="<?= base_url() ?>assets/images/pizza10.png" alt="">
+                <button type="button">Añadir</button>
+            </li>
+        </ul>
+    </div>
+</div>
