50c5e176e31a4fa8cc2c4fad158933e75fcffb8a
David Gomez <padawan@doersdf.com>
2017-05-23 15:40:04 +0200

    banner publi

--- application/views/carta/pizzas.php
+++ application/views/carta/pizzas.php
@@ -1,11 +1,21 @@
 <script type="text/javascript">
-    $(document).ready( function () {
-       $("#pizzaContainer li img").hover(function () {
-          $(this).next().show();
-       }, function () {
-          //mouseout
-          $(this).next().hide();
-       });
+    $(document).ready(function () {
+        $("#pizzaContainer li img").hover(function () {
+            var text = $(this).attr("src");
+            $(this).attr("src", text.replace(".png", "_t.png"));
+            console.log("mousein " + $(this).attr("src"));
+            $(this).next().show();
+        }, function () {
+            //mouseout
+            var text = $(this).attr("src");
+            $(this).attr("src", text.replace("_t.png", ".png"));
+            console.log("mouseoute " + $(this).attr("src"));
+            $(this).next().hide();
+        });
+
+        $("#cerrarPubli i").click(function () {
+            $("#bannerPizza").hide();
+        });
     });
 
 </script>
@@ -13,55 +23,80 @@
     <div class="row">
         <ul id="pizzaContainer">
             <li class="pizza" id="p1">
-                <img src="<?= base_url() ?>assets/images/pizza1.png" alt="">
-                <h1 hidden>TITULO DE LA PIZZA</h1>
-                <button type="button">Añadir</button>
+                <div class="imagenTitulo" style="">
+                    <img src="<?= base_url() ?>assets/images/pizza5.png" alt="">
+                    <h5 hidden>PERSONALIZADA</h5>
+                </div>
+                <button type="button" onclick="addPizza(1)">Añadir</button>
             </li>
             <li class="pizza" id="p2">
-                <img src="<?= base_url() ?>assets/images/pizza2.png" alt="">
-                <h1 hidden>TITULO DE LA PIZZA</h1>
-                <button type="button">Añadir</button>
+                <div class="imagenTitulo" style="">
+                    <img src="<?= base_url() ?>assets/images/pizza1.png" alt="">
+                    <h5 hidden>MARGARITA</h5>
+                </div>
+                <button type="button" onclick="addPizza(2)">Añadir</button>
             </li>
             <li class="pizza" id="p3">
-                <img src="<?= base_url() ?>assets/images/pizza3.png" alt="">
-                <h1 hidden>TITULO DE LA PIZZA</h1>
-                <button type="button">Añadir</button>
+                <div class="imagenTitulo" style="">
+                    <img src="<?= base_url() ?>assets/images/pizza2.png" alt="">
+                    <h5 hidden>COMPLETA</h5>
+                </div>
+                <button type="button" onclick="addPizza(3)">Añadir</button>
             </li>
             <li class="pizza" id="p4">
-                <img src="<?= base_url() ?>assets/images/pizza4.png" alt="">
-                <h1 hidden>TITULO DE LA PIZZA</h1>
-                <button type="button">Añadir</button>
+                <div class="imagenTitulo" style="">
+                    <img src="<?= base_url() ?>assets/images/pizza3.png" alt="">
+                    <h5 hidden>QUESERA</h5>
+                </div>
+                <button type="button" onclick="addPizza(4)">Añadir</button>
             </li>
             <li class="pizza" id="p5">
-                <img src="<?= base_url() ?>assets/images/pizza5.png" alt="">
-                <h1 hidden>TITULO DE LA PIZZA</h1>
-                <button type="button">Añadir</button>
+                <div class="imagenTitulo" style="">
+                    <img src="<?= base_url() ?>assets/images/pizza4.png" alt="">
+                    <h5 hidden>VEGETARIANA</h5>
+                </div>
+                <button type="button" onclick="addPizza(5)">Añadir</button>
             </li>
             <li class="pizza" id="p6">
-                <img src="<?= base_url() ?>assets/images/pizza6.png" alt="">
-                <h1 hidden>TITULO DE LA PIZZA</h1>
-                <button type="button">Añadir</button>
+                <div class="imagenTitulo" style="">
+                    <img src="<?= base_url() ?>assets/images/pizza6.png" alt="">
+                    <h5 hidden>PEPERONI</h5>
+                </div>
+                <button type="button" onclick="addPizza(6)">Añadir</button>
             </li>
             <li class="pizza" id="p7">
-                <img src="<?= base_url() ?>assets/images/pizza7.png" alt="">
-                <h1 hidden>TITULO DE LA PIZZA</h1>
-                <button type="button">Añadir</button>
+                <div class="imagenTitulo" style="">
+                    <img src="<?= base_url() ?>assets/images/pizza7.png" alt="">
+                    <h5 hidden>IBÉRICA</h5>
+                </div>
+                <button type="button" onclick="addPizza(7)">Añadir</button>
             </li>
             <li class="pizza" id="p8">
-                <img src="<?= base_url() ?>assets/images/pizza8.png" alt="">
-                <h1 hidden>TITULO DE LA PIZZA</h1>
-                <button type="button">Añadir</button>
+                <div class="imagenTitulo" style="">
+                    <img src="<?= base_url() ?>assets/images/pizza8.png" alt="">
+                    <h5 hidden>BARBACOA</h5>
+                </div>
+                <button type="button" onclick="addPizza(8)">Añadir</button>
             </li>
             <li class="pizza" id="p9">
-                <img src="<?= base_url() ?>assets/images/pizza9.png" alt="">
-                <h1 hidden>TITULO DE LA PIZZA</h1>
-                <button type="button">Añadir</button>
+                <div class="imagenTitulo" style="">
+                    <img src="<?= base_url() ?>assets/images/pizza9.png" alt="">
+                    <h5 hidden>CARBONARA</h5>
+                </div>
+                <button type="button" onclick="addPizza(9)">Añadir</button>
             </li>
             <li class="pizza" id="p10">
-                <img src="<?= base_url() ?>assets/images/pizza10.png" alt="">
-                <h1 hidden>TITULO DE LA PIZZA</h1>
-                <button type="button">Añadir</button>
+                <div class="imagenTitulo" style="">
+                    <img src="<?= base_url() ?>assets/images/pizza10.png" alt="">
+                    <h5 hidden>DIABÓLICA</h5>
+                </div>
+                <button id="btnMover" type="button" onclick="addPizza(10)">Añadir</button>
             </li>
         </ul>
     </div>
+    <div id="bannerPizza">
+        <a href="<?= base_url() ?>"><img src="<?= base_url() ?>assets/images/publi.png" alt=""></a>
+        <div id="cerrarPubli"><i class="fa fa-times" aria-hidden="true"></i></div>
+    </div>
 </div>
+
