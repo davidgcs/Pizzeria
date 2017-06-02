daa1f66ecaab01ed5b48007a3f953662c8047d6f
David Gomez <padawan@doersdf.com>
2017-06-02 14:53:01 +0200

    perfil

--- application/views/perfil/index.php
+++ application/views/perfil/index.php
@@ -1,4 +1,5 @@
 <style>footer{width: 100%;position: absolute;bottom: 0;}</style>
 <div class="container" style="margin: 5% 3%">
-    <h1>Esto es tu perfil de usuario.</h1>
+    <div id="avatar" style="background-image: url('<?php /*obtener imagen bbdd*/ echo "https://static1.squarespace.com/static/51a28b22e4b040da1a84238c/530ddc15e4b0238c30fa3845/530ddc1ee4b0238c30fa3847/1393417251056/ASTL9022-2.jpg";?>')"></div>
+    <button id="cambiarAvatar" type="button">Cambiar avatar</button>
 </div>
