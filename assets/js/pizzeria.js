7e13ac10a8367a73aff3a1914bfd27cdc3937702
Daniserra14 <Daniserra14@gmail.com>
2017-06-11 17:54:43 +0200

    cart

--- assets/js/pizzeria.js
+++ assets/js/pizzeria.js
@@ -3,4 +3,11 @@
  */
 $( document ).ready(function() {
     console.log( "Jquery is ready!" );
+
+    $("#header a.cartIcon i").on("click", function(){
+        $(this).toggleClass("iconClicked");
+        $("#header .cart").animate({
+            height: $("#header .cart").height()==0 ? "400px":"0"
+        }, 500);
+    });
 });
